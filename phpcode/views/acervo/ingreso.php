fd15d8d431f44e1b4d4eb7e894a9ae34d4a2f1dd
Diego Rodriguez <diego@qwavee.com>
2016-02-27 09:17:12 -0300

    Initial commit for Acervo UI refactorig

--- phpcode/views/acervo/ingreso.php
+++ phpcode/views/acervo/ingreso.php
@@ -4,6 +4,8 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 use kartik\tabs\TabsX;
 use yii\helpers\Url;
+use kartik\widgets\ActiveForm;
+
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Acervo */
@@ -15,29 +17,39 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="acervo-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    
+    <?php 
+        $form = ActiveForm::begin(
+            ['type'=>ActiveForm::TYPE_VERTICAL]
+        );
+    ?>
+    
 
-    <?=  TabsX::widget([
-    'position' => TabsX::POS_ABOVE,
-    'align' => TabsX::ALIGN_LEFT,
-        'encodeLabels'=>false,
-    'items' => [
-        [
-            'label' => 'Información <i class="glyphicon glyphicon-list-alt"></i>',
-            'content' => $this->render('_form', ['model' => $model]),
-            'active' => true,
-        ],
+    <?=  
+        TabsX::widget([
+            'position' => TabsX::POS_ABOVE,
+            'align' => TabsX::ALIGN_LEFT,
+            'encodeLabels'=>false,
+            'items' => [
+                [
+                    'label' => 'Información <i class="glyphicon glyphicon-list-alt"></i>',
+                    'content' => $this->render('_form', ['model' => $model, 'form' => $form]),
+                    'active' => true,
+                ],
 
-        [
-            'label' => 'Imaǵenes <i class="glyphicon glyphicon-picture"></i>',
-            'content' => $this->render('_formMultimedia', ['model' => $model, 'acervo_id' =>$model->id ]),
-            'options' => ['id' => 'myveryownID'],
-        ],
+                [
+                    'label' => 'Imágenes <i class="glyphicon glyphicon-picture"></i>',
+                    'content' => $this->render('_formMultimedia', ['model' => $model, 'acervo_id' =>$model->id, 'form' => $form ]),
+                    'options' => ['id' => 'myveryownID'],
+                ],
+            ],
+        ]);
+    ?>
 
-    ],
-]);
-?>
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
 
-
-   
-
+    <?php ActiveForm::end(); ?>
+    
 </div>
