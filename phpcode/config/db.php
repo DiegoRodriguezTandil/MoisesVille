d61e8851f9a51b5a6d23becd7c2b806f09717094
Alejandra Pizzi <alejandra@qwavee.com>
2018-05-18 09:46:03 -0300

    archivos de mas

--- phpcode/config/db.php
+++ phpcode/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;port=3308;dbname=moisesville',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=moises',
     'username' => 'root',
     'password' => 'mysqlpass',
     'charset' => 'utf8',
